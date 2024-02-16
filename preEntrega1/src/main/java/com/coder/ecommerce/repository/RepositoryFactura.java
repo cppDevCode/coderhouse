a46643cf1a6364f95e9a2962cd1fa7d1d0134c82
Alejandro Baldres <alejandro.baldres@gmail.com>
2024-02-16 20:09:52 -0300

    commit final

--- preEntrega1/src/main/java/com/coder/ecommerce/repository/RepositoryFactura.java
+++
@@ -1,9 +0,0 @@
-package com.coder.ecommerce.repository;
-
-import com.coder.ecommerce.models.Factura;
-import org.springframework.data.jpa.repository.JpaRepository;
-import org.springframework.stereotype.Repository;
-
-@Repository
-public interface RepositoryFactura extends JpaRepository<Factura, Long> {
-}
--- preEntrega1/src/main/java/com/coder/ecommerce/repository/RepositoryProducto.java
+++
@@ -1,9 +0,0 @@
-package com.coder.ecommerce.repository;
-
-import com.coder.ecommerce.models.Producto;
-import org.springframework.data.jpa.repository.JpaRepository;
-import org.springframework.stereotype.Repository;
-
-@Repository
-public interface RepositoryProducto extends JpaRepository<Producto,Long> {
-}
